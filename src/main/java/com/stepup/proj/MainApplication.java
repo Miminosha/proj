613eb0bf41c81471704c7ff5cba392d1a7fee8b8
Anna Memikova <anna.memikova@yandex.ru>
2025-12-01 22:18:13 +0300

    task102

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -3,12 +3,13 @@ package com.stepup.proj;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("+");
         System.out.println("Good morning!");
     }
 
     public static void doSomething() {
+        System.out.println(0);
         System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
+        System.out.println(1);
     }
 }
